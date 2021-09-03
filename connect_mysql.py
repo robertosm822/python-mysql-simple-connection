1ec22d3ba7090b1ca6bd1608623466bc054aaa04
Roberto Melo <robertomelo822@gmail.com>
2021-09-03 00:20:12 -0300

    delete
    delete

--- connect_mysql.py
+++
@@ -1,5 +0,0 @@
-
-import mysql.connector
-
-## Funciana em modo de prompt do Commander como administrador do Windows
-cnx = mysql.connector.connect(user='robertosm8222', password='87987469', host='db4free.net', database='sistema_rsm_2')
